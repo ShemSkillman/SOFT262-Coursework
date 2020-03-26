92ddc6aec45d0c14cf8f7a9005196c829d2b4142
ShemSkillman <43577362+ShemSkillman@users.noreply.github.com>
2020-03-26 15:46:59 +0000

    Finished UI for Creating cards tab
    Completed tasks:
    - Grid layout for create cards tab with labels, input fields and buttons

--- SOFT262/SOFT262/SOFT262/TabbedPages/Creation/CreationViewModel.cs
+++ SOFT262/SOFT262/SOFT262/TabbedPages/Creation/CreationViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using SOFT262.Model;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace SOFT262.Creation
@@ -8,6 +9,20 @@ namespace SOFT262.Creation
     public class CreationViewModel
     {
         MainModel model;
+
+        int topicIndex;
+
+        public int TopicIndex
+        {
+            get => topicIndex;
+            set
+            {
+                if (topicIndex == value) return;
+
+                topicIndex = value;
+            }
+        }
+
         public CreationViewModel(MainModel model)
         {
             this.model = model;
