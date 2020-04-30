a50a04f0fbe45d3257fe9d45c235e104f8106f05
LewisJones30 <43572935+LewisJones30@users.noreply.github.com>
2020-04-30 12:55:51 +0100

    summary page changes

--- SOFT262/SOFT262/SOFT262/TabbedPages/Summary/SummaryPage.xaml.cs
+++ SOFT262/SOFT262/SOFT262/TabbedPages/Summary/SummaryPage.xaml.cs
@@ -14,16 +14,23 @@ namespace SOFT262.Summary
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class SummaryPage : ContentPage, IPageHelper
     {
+        SummaryViewModel vm;
+        
         public SummaryPage()
         {
             InitializeComponent();
-
-            BindingContext = new SummaryViewModel(this);
+            vm = new SummaryViewModel(this);
+            BindingContext = vm;
         }
 
         public Task MessagePopup(string messageTitle, string message)
         {
             throw new NotImplementedException();
+        }
+
+        private void listTopics_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            vm.TopicChosen();
         }
     }
 }
